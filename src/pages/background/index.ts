d7ba6fe07ad459b6e4dc2b7e6cb5d0834b138dcb
0xngmi <0xngmi@protonmail.com>
2023-04-24 01:56:05 +0100

    remove unused code

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -14,82 +14,8 @@ import {
   DEFILLAMA_DIRECTORY_API,
   PROTOCOL_TVL_THRESHOLD,
 } from "../libs/constants";
-import { getStorage } from "../libs/helpers";
-import { checkDomain } from "../libs/phishing-detector";
 
 startupTasks();
-
-async function getCurrentTab() {
-  const queryOptions = { active: true, currentWindow: true };
-  const [tab] = await Browser.tabs.query(queryOptions);
-  return tab;
-}
-
-async function handlePhishingCheck() {
-  const phishingDetector = await getStorage("local", "settings:phishingDetector", true);
-  if (!phishingDetector) {
-    Browser.action.setIcon({ path: cute });
-    return;
-  }
-
-  let isPhishing = false;
-  let isTrusted = false;
-  let reason = "Unknown website";
-  const tab = await getCurrentTab();
-  try {
-    const url = tab.url;
-    if (url.startsWith("https://metamask.github.io/phishing-warning")) {
-      // already captured and redirected to metamask phishing warning page
-      isPhishing = true;
-      reason = "Phishing detected by Metamask";
-    } else {
-      const domain = new URL(url).hostname.replace("www.", "");
-      const res = await checkDomain(domain);
-      console.log("checkDomain", res);
-      isPhishing = res.result;
-      if (isPhishing) {
-        switch (res.type) {
-          case "blocked":
-            reason = "Website is blacklisted";
-            break;
-          case "fuzzy":
-            reason = `Website impersonating ${res.extra}`;
-            break;
-          default:
-            reason = "Suspicious website detected";
-        }
-      } else {
-        switch (res.type) {
-          case "allowed":
-            isTrusted = true;
-            reason = "Website is whitelisted";
-            break;
-          default:
-            reason = "Unknown website";
-        }
-      }
-    }
-  } catch (error) {
-    console.log("handlePhishingCheck error", error);
-    isTrusted = false;
-    isPhishing = false;
-    reason = "Invalid URL";
-  }
-
-  if (isTrusted) {
-    Browser.action.setIcon({ path: upOnly });
-    Browser.action.setTitle({ title: reason });
-    return;
-  }
-
-  if (isPhishing) {
-    Browser.action.setIcon({ path: maxPain });
-    Browser.action.setTitle({ title: reason });
-  } else {
-    Browser.action.setIcon({ path: que });
-    Browser.action.setTitle({ title: reason });
-  }
-}
 
 export async function updateNftDb() {
   const raw = await fetch(`https://nft.llama.fi/collections`).then((res) => res.json());
@@ -204,15 +130,6 @@ export async function updateDomainDbs() {
 
   console.log("updateDomainDbs", "done");
 }
-
-Browser.tabs.onUpdated.addListener(async () => {
-  console.log("onUpdated");
-  await handlePhishingCheck();
-});
-Browser.tabs.onActivated.addListener(async () => {
-  console.log("onActivated");
-  await handlePhishingCheck();
-});
 
 function setupUpdateProtocolsDb() {
   console.log("setupUpdateProtocolsDb");
--- src/pages/libs/helpers.ts
+++ src/pages/libs/helpers.ts
@@ -1,7 +1,6 @@
 import Browser from "webextension-polyfill";
 
 import { ACCOUNTS_API_V2, Prices, PRICES_API, TagsDataV2 } from "./constants";
-import { ICONS } from "./tagging-helpers";
 
 export const getIsMac = () => /(Mac|iPhone|iPod|iPad)/i.test(navigator?.platform);
 
@@ -95,13 +94,6 @@ export const getImageUrl = (url: string) => {
   if (url.startsWith("data:image")) return url;
   if (url.startsWith("https")) return url;
   return Browser.runtime.getURL(url);
-};
-
-export const getTagIconUrl = (link: string) => {
-  if (ICONS[link]) return Browser.runtime.getURL(ICONS[link]);
-  if (link.startsWith("data:image")) return link;
-  if (link.startsWith("https")) return link;
-  return Browser.runtime.getURL(link);
 };
 
 export function createInlineLlamaIcon(src: string, alt: string, size = 12, className = "mr-1 mCS_img_loaded") {
