569cd7d5ff5e5efdf3ef971a051fce1230fd3817
mintdart <96025197+mintdart@users.noreply.github.com>
2023-04-25 00:33:06 -0500

    update styles

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -97,7 +97,7 @@ const Popup = () => {
                 },
               );
             }}
-            className="py-2 px-4 bg-blue-100 hover:bg-blue-200 mx-auto text-black w-fit text-base font-medium rounded-lg"
+            className="py-2 px-4 bg-blue-100 hover:bg-blue-200 mx-auto text-black w-fit text-base font-medium rounded-md"
           >
             Display top used sites
           </button>
@@ -105,10 +105,10 @@ const Popup = () => {
 
         {displayTopSites && (
           <>
-            <h2 className="text-xl font-semibold text-center">
-              Top most visited sites{" "}
+            <h2 className="text-xl font-semibold text-center flex items-center gap-4 justify-center">
+              <span>Top most visited sites</span>
               <button
-                className="py-2 px-4 bg-blue-100 hover:bg-blue-200 mx-auto text-black w-fit text-base font-medium rounded-lg"
+                className="py-1 px-3 bg-gray-100 hover:bg-gray-200 text-black text-base font-medium rounded-md"
                 onClick={() => {
                   chrome.permissions.remove(
                     {
@@ -123,15 +123,33 @@ const Popup = () => {
                   );
                 }}
               >
-                x
+                <span className="sr-only">Hide</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                  <path
+                    fillRule="evenodd"
+                    d="M3.28 2.22a.75.75 0 00-1.06 1.06l14.5 14.5a.75.75 0 101.06-1.06l-1.745-1.745a10.029 10.029 0 003.3-4.38 1.651 1.651 0 000-1.185A10.004 10.004 0 009.999 3a9.956 9.956 0 00-4.744 1.194L3.28 2.22zM7.752 6.69l1.092 1.092a2.5 2.5 0 013.374 3.373l1.091 1.092a4 4 0 00-5.557-5.557z"
+                    clipRule="evenodd"
+                  />
+                  <path d="M10.748 13.93l2.523 2.523a9.987 9.987 0 01-3.27.547c-4.258 0-7.894-2.66-9.337-6.41a1.651 1.651 0 010-1.186A10.007 10.007 0 012.839 6.02L6.07 9.252a4 4 0 004.678 4.678z" />
+                </svg>
               </button>
             </h2>
 
-            <ul className="flex flex-col gap-2">
+            <ul className="flex gap-2 flex-wrap mx-auto justify-center">
               {displayTopSites.map((o) => (
-                <li key={"top-sites" + o.url} className="text-center">
-                  <a href={o.url} className="text-base text-center underline" rel="noopener noreferrer">
-                    {o.title}
+                <li key={"top-sites" + o.url} className="rounded-md hover:bg-gray-100 p-2">
+                  <a
+                    href={o.url}
+                    className="text-xs flex flex-col gap-2 w-[80px] justify-between p-2"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="mx-auto p-2 bg-gray-200 rounded-md">
+                      <img
+                        src={`https://www.google.com/s2/favicons?domain_url=${o.url}&sz=64`}
+                        className="mx-auto object-contain w-10 h-10"
+                      />
+                    </span>
+                    <span className="text-ellipsis overflow-hidden whitespace-nowrap text-center">{o.title}</span>
                   </a>
                 </li>
               ))}
