2dde62a649f9400639e72ba3440d77487b097bd7
0xngmi <0xngmi@protonmail.com>
2023-10-05 01:33:05 +0100

    change ranking algo

--- src/pages/search/index.ts
+++ src/pages/search/index.ts
@@ -15,6 +15,7 @@ interface Token {
     [chain: string]: string
   }
   symbol: string;
+  mcap: number;
 }
 
 interface NFT {
@@ -30,12 +31,20 @@ interface Other {
     logo: string;
 }
 
+function calculateRankingScore(searchText:string, name:string, usdValue:number, altKey?:string){
+    if(name.toLowerCase().startsWith(searchText)){
+        return usdValue
+    }
+    return 0
+}
+
 export function calculateSearchOptions(text:string, storedDB: {tokens:Token[], nfts:NFT[], protocols:Protocol[], other: Other[]}){
     const normalizedText = text.toLowerCase()
     const googleSearchOption = {
         text: "Search google",
         url: `https://www.google.com/search?q=${text}`,
-        type: "google"
+        type: "google",
+        score: 1
     }
     if(/0x[0-f]{64}/.test(text)){
         return [
@@ -85,43 +94,35 @@ export function calculateSearchOptions(text:string, storedDB: {tokens:Token[], n
         ]
     }
     let list = []
-    if(text.length > 3){
-        const nftsMatched = storedDB.nfts.filter(p=>p.name.toLowerCase().includes(normalizedText))
-        if(nftsMatched.length>0){ // Match nft
-            const first = nftsMatched.sort((a,b)=>b.mcap-a.mcap)[0]
-            list = list.concat([
-                {
-                    text: `Check price of ${first.name} on defillama`,
-                    url: `https://defillama.com/nfts/collection/${first.collectionId}`,
-                    type: "nft"
-                },
-                {
-                    text: `Open ${first.name} on Opensea Pro`,
-                    url: `https://pro.opensea.io/collection/${first.collectionId}`,
-                    type: "nft"
-                }
-            ])
-        }
-        const protocolsMatched = storedDB.protocols.filter(p=>p.name.toLowerCase().includes(normalizedText))
-        if(protocolsMatched.length>0){ // Match protocol
-            list = list.concat(protocolsMatched.sort((a,b)=>b.tvl-a.tvl).map(p=>({
+    if (text.length > 3) {
+        list = list.concat(
+            storedDB.nfts.map(p => ({
+                text: `Check price of ${p.name} on defillama`,
+                url: `https://defillama.com/nfts/collection/${p.collectionId}`,
+                type: "nft",
+                score: calculateRankingScore(normalizedText, p.name, p.mcap)
+            })),
+            storedDB.protocols.map(p => ({
                 text: `Open ${p.name}`,
                 url: p.url,
-                type: "protocol"
-            })))
-        }
-    }
-    if(text.length > 2){
-        const matched = storedDB.tokens.filter(p=>p.name.toLowerCase().includes(normalizedText) || p.symbol.includes(normalizedText))
-        if(matched.length>0){ // Match protocol
-            list = list.concat(matched.slice(0,3).map(p=>({
+                type: "protocol",
+                score: calculateRankingScore(normalizedText, p.name, p.tvl)
+            })),
+            storedDB.other.map(p => ({
+                text: `Open ${p.name}`,
+                url: p.url,
+                type: "other",
+                score: calculateRankingScore(normalizedText, p.name, Number.MAX_VALUE)
+            })),
+            storedDB.tokens.map(p => ({
                 text: `Check ${p.name} on coingecko`,
                 url: `https://www.coingecko.com/en/coins/${p.id}`,
-                type: "coingecko"
-            })))
-        }
+                type: "coingecko",
+                score: calculateRankingScore(normalizedText, p.name, p.mcap, p.symbol)
+            }))
+        )
     }
-    return list.concat([
+    return list.sort((a,b)=>b.score - a.score).slice(0,4).concat([
         googleSearchOption
     ])
     // Swap X for Y
--- src/pages/search/search.test.ts
+++ src/pages/search/search.test.ts
@@ -9,6 +9,8 @@ describe.each([
     ["$BT", "$BTC on coingecko"],
     ["bored ape", nft("Bored Ape Yacht Club")],
     ["bitcoin", gc("Bitcoin")],
+    ["binance", "Open Binance Exchange"],
+    ["Curve", "Open Curve Finance"]
 ])('search(%s)', (searchString, expected) => {
     test(`returns "${expected}"`, () => {
       expect(ss(searchString)[0].text).toBe(expected);
